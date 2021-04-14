f4486221a256bd852dfab272f5fa29583fc949ac
densec <densec@densecs-iMac.local>
2021-04-14 14:12:13 +0530

    responsive sidebar added...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 import "./Header.css";
+import { BiMenu } from "react-icons/bi";
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
 function Header() {
   const [show, setShow] = useState(false);
   const toggleNav = () => {
@@ -7,43 +16,29 @@ function Header() {
   };
   console.log(show);
   return (
-    <div
-      style={{ backgroundColor: `${show ? "white" : ""}` }}
-      className="header"
-    >
-      <div className="top">
-        <div className="headerLeft">
-          <img
-            href="#section-1"
-            src="https://cdn.worldvectorlogo.com/logos/tesla-9.svg"
-            alt=""
-          />
-        </div>
-        <div className="headerCenter">
-          <a href="#section-1">MODEL S</a>
-          <a href="#section-2">MODEL 3</a>
-          <a href="#section-3">MODEL X</a>
-          <a href="#section-4">MODEL Y</a>
-        </div>
-        <div className="headerRight">
-          <a href="#section-1">Shop</a>
-          <a href="">Tesla account</a>
-          <img
-            onClick={toggleNav}
-            style={{ height: "25px", marginLeft: "15px" }}
-            src="./menu.png"
-            alt=""
-          />
-        </div>
+    <div className="header">
+      <div>
+        <img src="https://cdn.worldvectorlogo.com/logos/tesla-9.svg" alt="" />
+      </div>
+      <div className="center">
+        <a href="#section-1">Model S</a>
+        <a href="#section-2">Model 3</a>
+        <a href="#section-3">Model X</a>
+        <a href="#section-4">Model Y</a>
+      </div>
+      <div className="right">
+        <a href="#">Shop</a>
+        <a href="#">tesla account</a>
+      </div>
+      <div onClick={toggleNav} className="menuIcon">
+        <BiMenu size={30} />
       </div>
       {show && (
-        <div className="bottom">
-          <a href="#section-1">MODEL S</a>
-          <a href="#section-2">MODEL 3</a>
-          <a href="#section-3">MODEL X</a>
-          <a href="#section-4">MODEL Y</a>
-          <a href="#section-1">Shop</a>
-          <a href="">Tesla account</a>
+        <div className="sideNav">
+          <a href="#section-1">Model S</a>
+          <a href="#section-2">Model 3</a>
+          <a href="#section-3">Model X</a>
+          <a href="#section-4">Model Y</a>
         </div>
       )}
     </div>
